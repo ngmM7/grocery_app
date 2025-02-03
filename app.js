3d4980a73dd3dbac01e31473d5d90da54587fe08
ngmM7 <nagimashir@gmail.com>
2025-02-03 14:53:49 +0100

    add app.js file addItem function

--- app.js
+++ app.js
@@ -5,3 +5,54 @@ const submitBtn = document.querySelector(".submit-btn");
 const container = document.querySelector(".grocery-container");
 const list = document.querySelector(".grocery-list");
 const clearBtn = document.querySelector(".clear-btn");
+
+// adit option
+
+let editElement;
+let editFlag = false;
+let editID = "";
+
+// *** event listeners ****
+
+// submit form
+
+form.addEventListener("submit", addItem);
+
+// clear list
+
+clearBtn.addEventListener("click", clearItem);
+
+// display items onload
+
+window.addEventListener("DOMContentLoaded", setupItems);
+
+// *** function ****
+
+// add item
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getItem().toString()
+
+
+    if (value !== "" && !editFlag) {
+        const element = document.createElement("article");
+        let attr = document.createAttribute("data-id");
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.classList.add("grocery-item");
+        element.innerHTML = ` <p class="title">${value}</p>
+                <div class="btn-container">
+                <!-- edit btn -->
+                <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+                </button>
+                <! -- delete btn -- >
+                <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+                </button>
+                </div>
+        `;
+
+    }
+}
